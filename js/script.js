48ad6746264a8ef28c5415164db19ed8fb5caa45
Gustavo Feriani <guuh.feriani@gmail.com>
2023-10-28 16:11:07 -0300

    update JavaScrit
    upload do projeto 28/10

--- js/script.js
+++ js/script.js
@@ -6,12 +6,24 @@
 // tres(3) constantes para referenciar os ID das DIVS no Projetos
 
 const album = document.getElementById('album');
+const casamentos = document.getElementById('casamentos');
+const buffet = document.getElementById('buffet');
+
 // tres(3) constantes para referenciar os ID dos iFrames 
 const videoAlbum = document.querySelector('#videoAlbum');
+const videoCasamentos = document.querySelector('#videoCasamentos');
+const videoBuffet = document.querySelector('#videoBuffet');
 
 // evento do Click 
 album.addEventListener('click', () => {
     album.classList.toggle('active');  
     videoAlbum.classList.toggle('active');
 });
-
+casamentos.addEventListener('click', () => {
+    casamentos.classList.toggle('active');
+    videoCasamentos.classList.toggle('active');
+});
+buffet.addEventListener('click', () => {
+    buffet.classList.toggle('active');
+    videoBuffet.classList.toggle('active');
+});
